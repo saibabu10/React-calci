a5b5991192bcc7cfb43dd32b060b5bb6960869fb
sai babu <saibabu@sais-Air-2.domain.name>
2023-04-11 23:21:28 +0530

    added calculator in forms with function

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
   return (
     <Router>
     <div className='App'>
-     <Link to="/Portfolio">Portfolio</Link>
+     <Link to="/">Portfolio</Link>
      <Link to="/Calculator">Calculator</Link>
      <Link to="/Cartoon">Axios</Link>
     </div>
     <Routes>
-      <Route exact path='/Portfolio' element={<Portfolio/>}></Route>
+      <Route exact path='/' element={<Portfolio/>}></Route>
       <Route exact path='/Calculator' element={<Calculator/>}></Route>
       <Route exact path='/Cartoon' element={<Cartoon/>}></Route>
     </Routes>
--- src/Component/Routes/Calculator Component/Calculator.jsx
+++ src/Component/Routes/Calculator Component/Calculator.jsx
@@ -1,92 +1,79 @@
-import React from 'react';
-import './Calculator.css'
-function Calculator(props)
- {
-
-// const [input, setInput] = React.useState("0");
-
-// const handleNumbers = (event) => {
-//   setInput(event.target.value);
-// };
-
-return (
-  <div>
-    <div className="calculator">
-      <History />
-      { <Output/>/*currentValue={input} /> */}
-      <Buttons />
-    </div>
-  </div>
-);
-}
-
-const History = () => {
-return <div id="history"> </div>;
-};
-
-const Output = (props) => {
-return <div id="display">{props.currentValue}</div>;
-};
-
-const Buttons = () => {
-return (
-  <div className="buttons">
-    <button id="clear" value="AC" className="jumbo">
-      AC
-    </button>
-    <button id="divide" value="/">
-      /
-    </button>
-    <button id="multiply" value="x">
-      x
-    </button>
-    <button id="seven" value="7">
-      7
-    </button>
-    <button id="eight" value="8">
-      8
-    </button>
-    <button id="nine" value="9">
-      9
-    </button>
-    <button id="subtract" value="-">
-      -
-    </button>
-    <button id="four" value="4">
-      4
-    </button>
-    <button id="five" value="5">
-      5
-    </button>
-    <button id="six" value="6">
-      6
-    </button>
-
-    <button id="add" value="+">
-      +
-    </button>
-
-    <button id="one" value="1">
-      1
-    </button>
-    <button id="two" value="2">
-      2
-    </button>
-    <button id="three" value="3">
-      3
-    </button>
-    <button id="equals" value="=">
-      =
-    </button>
-      <button id="zero" value="0" className="jumbo">
-        0
-      </button>
-
-      <button id="decimal" value=".">
-        .
-      </button>
-    </div>
-)
+import React, { Component } from 'react'
+import './Calculator.css';
+export class Calculator extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        expression:"", 
+      };
+    }
+    numberIsPressed = (event) =>
+    {
+      let inputString = this.state.expression + event.currentTarget.value;
+      this.setState({expression: inputString}); 
+    };
+    operatorIsPressed = (event) =>
+    {
+        let inputString = this.state.expression;
+        if(inputString !== "" && inputString.length>0)
+        {
+            let lastIndexChar = inputString.charAt(inputString.length-1);
+            if(lastIndexChar !== event.currentTarget.value && lastIndexChar.match(/^([0-9])$/))
+            {
+            inputString = this.state.expression + event.currentTarget.value;
+            this.setState({expression:inputString});
+            }
+        }
+    };
+    calculateTheResult = () =>
+    {
+        let inputExpression = this.state.expression;
+        if(inputExpression !== "" && inputExpression.length > 0)
+        {
+          let lastIndexChar = inputExpression.charAt(inputExpression.length-1);
+          if(!lastIndexChar.match(/^([0-9])$/))
+          {
+inputExpression = inputExpression.substring(0,inputExpression.length-1);
+          }
+          let finalOutput = eval(inputExpression);
+          this.setState({expression:finalOutput.toString()});
+        }
+    };
+    clearButton = () =>
+    {
+        this.setState({expression: ""});
+    };
+  render() {
+    return (
+      <form className='border'>
+        <div className='text'>
+            <br/>
+            <input type='textbox' value={this.state.expression}/>
+        </div>
+        <div className='button'>
+            <button value="1" onClick={this.numberIsPressed}>1</button>
+            <button value="2" onClick={this.numberIsPressed}>2</button>
+            <button value="3" onClick={this.numberIsPressed}>3</button>
+            <button value="4" onClick={this.numberIsPressed}>4</button>
+            <button value="5" onClick={this.numberIsPressed}>5</button>
+            <button value="6" onClick={this.numberIsPressed}>6</button>
+            <button value="7" onClick={this.numberIsPressed}>7</button>
+            <button value="8" onClick={this.numberIsPressed}>8</button>
+            <button value="9" onClick={this.numberIsPressed}>9</button>
+            <button value="0" onClick={this.numberIsPressed}>0</button>
+            <button value="+" onClick={this.operatorIsPressed}>+</button>
+            <button value="-" onClick={this.operatorIsPressed}>-</button>
+            <button value="*" onClick={this.operatorIsPressed}>*</button>
+            <button value="/" onClick={this.operatorIsPressed}>/</button>
+            <button value="Clear" onClick={this.clearButton}>CLR</button>
+            {/* <button value="Delete" onClick={}>DEL</button> */}
+            {/* <button value="dot" onClick={}>.</button> */}
+            <button value="Equalto" onClick={this.calculateTheResult}>=</button>
+        </div>
+      </form>
+    )
   }
+}
 
 export default Calculator
